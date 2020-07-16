9fadebf849215ea0efb6ebe81c5c18739bbe6129
bebyx <bebyx@protonmail.com>
2020-07-16 14:37:53 +0300

    Update sitemap_gen script and sitemap.xml to add lastmod date; add sitemap.xml link to robots.txt

--- sitemap_gen.sh
+++ sitemap_gen.sh
@@ -2,7 +2,7 @@
 
 #A script to generate sitemap.xml
 
-find . -name '*.html' -not -name '404.html' | sed "s|\./|https://bebyx\.co\.ua/|g" | sed "s|/index\.html||g" > out
+find . -name '*.html' -not -name '404.html' -printf '%p %TY-%Tm-%Td \n' | sed "s|\./|https://bebyx\.co\.ua/|g" | sed "s|/index\.html||g" > out
 
 echo '<?xml version="1.0" encoding="UTF-8"?>' > sitemap.xml
 echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' >> sitemap.xml
@@ -10,7 +10,8 @@ echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' >> sitemap.x
 while read LINE
   do
     echo '\t<url>' >> sitemap.xml
-    echo "\t\t<loc>$LINE</loc>" >> sitemap.xml
+    echo "\t\t<loc>`echo "$LINE" | cut -d " " -f1`</loc>" >> sitemap.xml
+    echo "\t\t<lastmod>`echo "$LINE" | cut -d " " -f2`</lastmod>" >> sitemap.xml
     echo '\t</url>' >> sitemap.xml
 done < out
 
